139c67522bd0f44baa7e211b92b21671e93d3cc0
Chad Kaplan <chadkaplan@knights.ucf.edu>
2022-10-07 23:28:12 -0400

    Fixed App.js not rendering properly.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./components/Main";
+import Create from "./components/Create";
+import Play from "./components/Play";
 
-const App = () => {
+const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" exact element={<Main />} />
       <Route path="/create" element={<Create />} />
       <Route path="/play/:id" element={<Play />} />
     </Routes>
-  </BrowserRouter>;
-};
+  </BrowserRouter>
+);
 
 export default App;
