2f338fb68f3a0ee41fa113113b2c120c7054d069
Chad Kaplan <chadkaplan@knights.ucf.edu>
2022-10-08 23:51:56 -0400

    Added a navbar.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./components/Main";
 import Create from "./components/Create";
 import Play from "./components/Play";
+import Navbar from "./components/Navbar";
 
 const App = () => (
   <BrowserRouter>
+    <Navbar />
     <Routes>
       <Route path="/" exact element={<Main />} />
       <Route path="/create" element={<Create />} />
