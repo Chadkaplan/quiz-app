b632f873998c0f495b33a8ce377244b6a1907a65
Chad Kaplan <chadkaplan@knights.ucf.edu>
2022-10-10 23:46:24 -0400

    Added simple input form to create component.

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Create = () => {
   return (
-    <div>Create</div>
+    <div>
+      <h1>Create A Quiz in Seconds</h1>
+      <form>
+        <label>Quiz Name
+          <input type="text" name="name" placeholder='Your Quiz Name'></input>
+        </label>
+      </form>
+    </div>
   )
 }
 
