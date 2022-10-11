d28e7eddc94798272f3a6b309b6d05c158543c88
Chad Kaplan <chadkaplan@knights.ucf.edu>
2022-10-10 23:30:39 -0400

    Added button to create quiz page on /create.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div>
+      <Link to="/Create">
+      <button>Create a Quiz</button>
+      </Link>
+    </div>
   )
 }
 
 export default Main
+
