e7b598a55314166407b17a5cc814983cd64ac3b8
Chad Kaplan <chadkaplan@knights.ucf.edu>
2022-10-18 23:24:17 -0400

    Need to fix each answer choice map.

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import data from "../data/questions.json"
+import React from "react";
+import data from "../data/questions.json";
 
 const Question = () => {
-  const ListQuestions = data.map((question) =>
-  <div key={question.id}>
-  <h5>{question.Question}</h5>
-  <h5>{question.Choices}</h5>
-  </div>)
+  // const ListAnswers = data.Choices.map((choice) =>
+  // <p>{choice}</p>)
+  const ListQuestions = data.map((question) => (
+    <div key={question.id}>
+      <h5>{question.Question}</h5>
+      {data.Choices.map((choice) => (
+        <div>{choice}</div>
+      ))}
+    </div>
+  ));
+  console.log(data[0].Choices);
   return (
     <>
-    {ListQuestions}
+      {ListQuestions}
     </>
-    )
-  }
+  );
+};
 
-
-export default Question
+export default Question;
