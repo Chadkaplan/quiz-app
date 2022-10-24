4976df814f867d07227bf98b1816ea4ce74a0d44
Chad Kaplan <chadkaplan@knights.ucf.edu>
2022-10-23 23:49:25 -0400

    Need to figure out why choices arent mapping properly.

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import data from "../data/questions.json";
+import "./question.css";
 
 const Question = () => {
   // const ListAnswers = data.Choices.map((choice) =>
@@ -7,17 +8,29 @@ const Question = () => {
   const ListQuestions = data.map((question) => (
     <div key={question.id}>
       <h5>{question.Question}</h5>
-      {data.Choices.map((choice) => (
-        <div>{choice}</div>
-      ))}
+      {/* <button className="choice-btn">{question.Choices}</button> */}
     </div>
   ));
-  console.log(data[0].Choices);
+
   return (
-    <>
-      {ListQuestions}
-    </>
+    <div>
+      {data.map((question) => {
+        return (
+          <div key={question.id}>
+            <h2>{question.Question}</h2>
+            {data.Choices.map((choice, index) => {
+              return (
+                <div key={index}>
+                  <h2>{choice}</h2>
+                </div>
+              );
+            })}
+
+            <hr />
+          </div>
+        );
+      })}
+    </div>
   );
 };
-
 export default Question;
