a9ffd283ece59e1dd1820789b11eb17c13bd8af3
Chad Kaplan <chadkaplan@knights.ucf.edu>
2023-01-04 08:47:10 -0500

    Remove extraneous form file.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./components/Main";
 import Create from "./components/Create";
 import Play from "./components/Play";
-import Navbar from "./components/Navbar";
-import Forms from "./components/Forms";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => (
   <BrowserRouter>
@@ -13,7 +12,6 @@ const App = () => (
     <Routes>
       <Route path="/" exact element={<Main />} />
       <Route path="/create" element={<Create />} />
-      <Route path="/Forms" element={<Forms />} />
       {/* Add dynamic routing to quizzes :id */}
       <Route path="/play" element={<Play />} />
     </Routes>
